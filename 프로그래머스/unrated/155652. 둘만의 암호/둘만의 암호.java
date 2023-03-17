347cdb863ace1bba1a2a2166268ea1e876223cd0
Jin Ki Lee <63500833+lxxjxxki@users.noreply.github.com>
2023-03-18 08:05:46 +0900

    [unrated] Title: 둘만의 암호, Time: 14.12 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            int count = 1;
+            while(count <= index){
+                ++c;
+                if(c > 'z')
+                    c -= (int)('z'-'a'+1);
+                if(skip.contains(c + ""))
+                    continue;
+                else
+                    count++;
+            }
+            answer += c;
+        }
+        return answer;
+    }
+}
