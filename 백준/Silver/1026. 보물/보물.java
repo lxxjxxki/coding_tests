5f6221081f584903a219d0b516f6f5ff2d2aaf08
Jin Ki Lee <63500833+lxxjxxki@users.noreply.github.com>
2023-05-05 22:58:30 +0900

    [Silver IV] Title: 보물, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        Integer[] a = new Integer[n];
+        int[] b = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a, Collections.reverseOrder());
+        Arrays.sort(b);
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer += a[i] * b[i];
+        }
+        System.out.println(answer);
+    }
+}
